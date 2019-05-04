79801f45d91f1900e9803eb8e2733c7adb3fd19a
milomacphail <milomacphail@gmail.com>
2019-05-04 19:09:22 -0400

    Added entry summing

---
+++ SumEntries/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumEntries
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            var sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number (or 'ok' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                    break;
+
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("Sum of all numbers is: " + sum);
+        }
+    }
+}
